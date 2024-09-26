7d4bf1bf7bdeef1b2dae737a19511b1fd411016d
Eduardo Torres Davila <etdavila10@gmail.com>
2024-09-26 10:22:24 -0500

    Outline for pipeline of ingesting and processing data

---
+++ database/src/ingest_data.sh
@@ -0,0 +1,25 @@
+# Make sure you're running python
+# from the virtual environment
+
+# Get the updated data from kaggle
+# This will download a zip file
+#
+
+# Unzip the zip file
+
+# place extracted json file into data
+# directory and name it 'raw_data.json'
+
+# start the parse_json.py script
+# This will process the data and should
+# create a 'processed_data.json' in data
+# directory
+
+# run the 'schema.py' script
+# this will create the tables necessary
+# to load in the updated data
+
+# run a 'load_data.py' script
+# which will go through and load the
+# processed_data.json file into the
+# database tables
