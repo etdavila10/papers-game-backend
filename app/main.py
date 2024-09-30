4fdcd2e6ff548139fc829870c195d251d3b76cdd
Eduardo Torres Davila <etdavila10@gmail.com>
2024-09-29 23:10:14 -0500

    Responding with two articles

--- app/main.py
+++ app/main.py
@@ -12,23 +12,17 @@ def read_root():
 
 @app.get("/random")
 def read_articles():
-
-    # should probably change location of database
     conn = sqlite3.connect('../database/src/database.db')
-
     cursor = conn.cursor()
-
     cursor.execute('SELECT identifier FROM paper_ids ORDER BY RANDOM() LIMIT 2')
 
     paper_ids = [row[0] for row in cursor.fetchmany(2)]
-    print(paper_ids)
 
     conn.close()
-
 
     response_dict = arxiv.call_arxiv_api(paper_ids)
 
     # List of dictionaries
     articles_list = response_dict["feed"]["entry"]
 
-    return articles_list[0]
+    return articles_list
