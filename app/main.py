02b76334b0c0c15553fa11f5d33807e237dc0c5a
Eduardo Torres Davila <etdavila10@gmail.com>
2024-09-30 05:46:53 +0000

    Setting up to use api keys

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 
 from . import arxiv
 import json
@@ -6,12 +6,18 @@ import sqlite3
 
 app = FastAPI()
 
+API_KEY = "test123"
+
+def validate_api_key(api_key: str):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=403, detail="Forbidden")
+
 @app.get("/")
-def read_root():
+def read_root(api_key: str = Depends(validate_api_key)):
     return {"Hello": "World"}
 
 @app.get("/random")
-def read_articles():
+def read_articles(api_key: str = Depends(validate_api_key)):
     conn = sqlite3.connect('database/src/database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT identifier FROM paper_ids ORDER BY RANDOM() LIMIT 2')
