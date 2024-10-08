f45ba8737bdce74fc6c15ce2ca094d754dfb719a
Eduardo Torres Davila <etdavila10@gmail.com>
2024-10-07 23:07:43 -0500

    Updated with Redis cache for quicker responses

--- app/arxiv.py
+++ app/arxiv.py
@@ -2,10 +2,11 @@ import requests
 import xmltodict
 import json
 
-base_url = 'https://export.arxiv.org/api/query?id_list='
+base_url = "https://export.arxiv.org/api/query?max_results=200&id_list="
+
 
 def call_arxiv_api(paper_ids):
-    full_url = base_url + ','.join(paper_ids)
+    full_url = base_url + ",".join(paper_ids)
     response = requests.get(full_url)
 
     data_dict = xmltodict.parse(response.content)
--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from redis.asyncio import Redis
+import asyncio
 
 from . import arxiv
 import json
 import sqlite3
 
 app = FastAPI()
+redis = None
+
+num_of_articles = 200
 
 origins = [
     "http://localhost",
+    "http://localhost:3000",
     "http://localhost:8080",
-    "https://tranquil-starship-92a3ed.netlify.app/"
+    "https://tranquil-starship-92a3ed.netlify.app/",
 ]
 
 app.add_middleware(
@@ -18,20 +24,27 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+"""
+    This should be the function that actually loads articles
+    into the Redis Cache. That way I can call it from
+    multiple locations in the code as a helper function.
+"""
 
-@app.get("/random")
-def read_articles():
-    conn = sqlite3.connect('database/src/database.db')
+
+async def load_articles(num_articles):
+
+    conn = sqlite3.connect("database/src/database.db")
+
     cursor = conn.cursor()
-    cursor.execute('SELECT identifier FROM paper_ids ORDER BY RANDOM() LIMIT 2')
 
-    paper_ids = [row[0] for row in cursor.fetchmany(2)]
+    cursor.execute(
+        f"SELECT identifier FROM paper_ids ORDER BY RANDOM() LIMIT {num_articles}"
+    )
+
+    paper_ids = [row[0] for row in cursor.fetchmany(num_articles)]
 
     conn.close()
 
@@ -40,4 +53,75 @@ def read_articles():
     # List of dictionaries
     articles_list = response_dict["feed"]["entry"]
 
-    return articles_list
+    # Add data to redis cache
+    for id, article in zip(paper_ids, articles_list):
+        await redis.set(id, json.dumps(article))
+
+
+async def check_sufficient_articles():
+    while True:
+        print("Checking something in the background...")
+
+        num_keys = await redis.dbsize()
+        if num_keys < (num_of_articles // 2):
+            articles_fill = num_of_articles - num_keys
+            await load_articles(articles_fill)
+            print(f"{articles_fill} articles were added")
+
+        await asyncio.sleep(30)
+
+
+"""
+    Want to set up redis cache so that articles
+    can be retrieved instantly from requests
+"""
+
+
+@app.on_event("startup")
+async def startup_event():
+
+    global redis
+    redis = Redis(host="localhost", port=6379, decode_responses=True)
+
+    # Make sure to remove any articles that were already there
+    await redis.flushdb()
+
+    await load_articles(num_of_articles)
+
+    asyncio.create_task(check_sufficient_articles())
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await redis.close()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    key = f"item:{item_id}"
+    cached_data = await redis.get(key)
+    if cached_data:
+        return json.loads(cached_data)
+    else:
+        return {"error": "Item not found"}
+
+
+@app.get("/random")
+async def read_articles():
+    random_key = await redis.randomkey()
+    cached_data = await redis.get(random_key)
+    # remove this from the article pool
+    await redis.delete(random_key)
+
+    # Get a second article
+    random_key_2 = await redis.randomkey()
+    cached_data_2 = await redis.get(random_key_2)
+    await redis.delete(random_key_2)
+
+    print(f"The article with id {random_key} was removed")
+    print(f"The article with id {random_key_2} was removed")
+
+    if cached_data and cached_data_2:
+        return [json.loads(article) for article in [cached_data, cached_data_2]]
+    else:
+        return {"error": "Item not found"}
